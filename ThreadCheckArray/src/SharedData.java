538ac0b1c44988afe2bf0c180d0a95f854447222
Ester Peretz <ester.peretz99@gmail.com>
2025-11-08 21:03:59 +0200

    add javadoc

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -1,21 +1,47 @@
 
+/**
+ * @author [Ester Peretz]
+ */
+/**
+ * 
+ */
 public class SharedData 
 {
+	
 	private int [] array;
 	private boolean [] winArray;
 	private boolean flag;
 	private final int b;
 	
+	/**
+	 * 
+	 * @param array hgjhgj
+	 * @param b dfgdgffd
+	 * 
+	 * 
+	 * **/
 	public SharedData(int[] array, int b) {
 		
 		this.array = array;
 		this.b = b;
 	}
+	/**
+	 * 
+	 * @return sgfdgfd
+	 * 
+	 * 
+	 * **/
 
 	public boolean[] getWinArray() 
 	{
 		return winArray;
 	}
+	/**
+	 * 
+	 * 
+	 * @param winArray vjhvjhvj
+	 * 
+	 * **/
 
 	public void setWinArray(boolean [] winArray) 
 	{	
@@ -23,21 +49,42 @@ public class SharedData
 		this.winArray = winArray;
 	}
 
+	/**
+	 * @return array
+	 */
 	public int[] getArray() 
 	{
 		System.out.println("testing");
 		return array;
 	}
+	/**
+	 * 
+	 * @return dgfdgfd
+	 * 
+	 * 
+	 * **/
 
 	public int getB() 
 	{
 		return b;
 	}
+	/**
+	 * 
+	 * @return fgfdgfd
+	 * 
+	 * 
+	 * **/
 
 	public boolean getFlag() 
 	{
 		return flag;
 	}
+	/**
+	 * 
+	 * @param flag    gjfgf 
+	 * 
+	 * 
+	 * **/
 
 	public void setFlag(boolean flag) {
 		this.flag = flag;
